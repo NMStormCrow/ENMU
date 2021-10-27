29ba0253a365b6767f11d89f219d3ecbbebf6065
Thomas Crow <paladin677@gmail.com>
2021-10-26 22:26:50 -0600

    Uploading CS123 Assignment 3 files

--- CS123/Assignments/Assignment #3/CS123_HW_3_Q_1_Crow_Thomas.java
+++ CS123/Assignments/Assignment #3/CS123_HW_3_Q_1_Crow_Thomas.java
@@ -12,9 +12,74 @@
 //                2       55 cents
 //                2.5     80 cents
 
-
+import java.lang.Math;
+import java.util.Scanner;
 
 class CurrencyPrettyPrint {
     public static void main(String args[]) {
+        
+        // Open Scanner
+        Scanner scan = new Scanner(System.in);
 
+        // Print out banner for user
+        for (int j=0; j<100; j++) 
+            System.out.print("*");
+        System.out.println();
+
+        System.out.print("***");
+        for (int j=0; j<94; j++) 
+            System.out.print(" ");
+        System.out.print("***");
+        System.out.println();
+        
+        System.out.print("***");
+        for (int j=0; j<30; j++) 
+            System.out.print(" ");
+        System.out.print("Shipping rates are as follows: ");    
+        for (int j=0; j<33; j++) 
+            System.out.print(" ");
+        System.out.print("***");
+        System.out.println();
+
+        System.out.print("***");
+        for (int j=0; j<30; j++) 
+            System.out.print(" ");
+        System.out.print("Airmail: First ounce is 30 cents: ");    
+        for (int j=0; j<30; j++) 
+            System.out.print(" ");
+        System.out.print("***");
+        System.out.println();
+
+        System.out.print("***");
+        for (int j=0; j<30; j++) 
+            System.out.print(" ");
+        System.out.print("Airmail: Additional ounces are 25 cents per ounce");    
+        for (int j=0; j<15; j++) 
+            System.out.print(" ");
+        System.out.print("***");
+        System.out.println();
+
+        System.out.print("***");
+        for (int j=0; j<94; j++) 
+            System.out.print(" ");
+        System.out.print("***");
+        System.out.println();
+        
+        for (int i=0; i<100; i++) 
+            System.out.print("*");
+        System.out.println();
+        System.out.println();
+        
+        // User Input
+        System.out.print("How many ounces is your letter? ");
+      
+        double letterWeight = scan.nextDouble();
+        int roundedLetterWeight = (int)Math.ceil(letterWeight);
+        double shippingCost = 0.3 + (0.25 * (roundedLetterWeight - 1));
+        System.out.println("The shipping cost is $" + String.format("%.2f",shippingCost));
+
+        // Close Scanner
+        scan.close();
+
+    }
 }
