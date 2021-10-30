8ee892dfbd520863afd211cfcc37ef430f926f84
Thomas Crow <paladin677@gmail.com>
2021-10-30 14:16:27 -0600

    Create files for CS120 Assignment #11

---
+++ CS120/Assignments/Assignment #11/Thomas_Crow_Assignment11-1.py
@@ -0,0 +1,24 @@
+#   Assignment #11 Question #1
+#
+#   Thomas Crow
+#   Date 10/30/2021
+#
+#   Requirements:
+#   The Goldbach conjecture asserts that every even number is the sum of two prime numbers.
+#               Wite a program that gets a number from the user, checks to make sure that it is even.
+#               and then finds two prime numbers that add up to the number
+   
+
+
+##################################################################################################################
+#  Description: 
+#  Input: None
+#  Output: None
+
+##################################################################################################################
+
+
+def main():
+    print()
+
+main()
---
+++ CS120/Assignments/Assignment #11/Thomas_Crow_Assignment11-2.py
@@ -0,0 +1,25 @@
+#   Assignment #11 Question #2
+#
+#   Thomas Crow
+#   Date 10/30/2021
+#
+#   Requirements: The greatest common divisor (GCD) of two values can be computed using Euclid's algorithm.
+#   Startomg with the values m and n, we repeatedly apply the formula: n, m = m, n % m until m is 0.   
+#   At that point, n is the GCD of the original m and n.
+#   Write a program that finds the GCD of two numbers using this algorithm.
+#
+
+##################################################################################################################
+#  Description: 
+#               
+#
+#  Input: None
+#  Output: None
+
+##################################################################################################################
+
+
+def main():
+    print()
+
+main()
