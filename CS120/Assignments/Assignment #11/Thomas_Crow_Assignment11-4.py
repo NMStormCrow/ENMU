2672f9323da7bd896e120de5224bc74415457fab
Thomas Crow <paladin677@gmail.com>
2021-11-08 00:01:10 -0700

    Completed 11-5, output

--- CS120/Assignments/Assignment #11/Thomas_Crow_Assignment11-4.py
+++ CS120/Assignments/Assignment #11/Thomas_Crow_Assignment11-4.py
@@ -10,19 +10,73 @@
 #                 file. The user signals the end of the trip with a blank line.
 #
 #                 The program should print out the miles per gallon achieved on
-#                 ech leg and the total MPG for the trip.
+#                 each leg and the total MPG for the trip.
 
 ##################################################################################################################
-#  Description: 
-#  Input: 
-#  Output:
+#  Description: Obtains the starting odometer reading from the user
+#  Input: None
+#  Output: Starting odometer reading 
 
+def obtainStartingOdometer():
+
+    startingOdometer = -1
+
+    while startingOdometer < 0:
+        startingOdometer = eval(input("Please enter the starting odometer value: "))
+        if (startingOdometer < 0):
+            print("Please enter a valid odometer value")
+    
+    return startingOdometer
+
+##################################################################################################################
+
+##################################################################################################################
+#  Description: Starting with a user inputted odometer value, this program reads from a file additional 
+#               odometer and fuel usage values
+#  Input: Start odometer value
+#  Output: None
+
+def calculateTripMileage(startOdometer):
+    
+    legOdd = []
+    legFuel = []
+    legDistance = []
+
+    currentOdometer = startOdometer
+    totalMileage = 0
+    totalFuel = 0
+
+    filename = input("Please enter the filename of the milage and fuel information: ")
+    infile = open(filename)
+    line = infile.readline()
+    line = line.strip()
+    while (line != ""):
+        
+        leginfo = line.split(',')
+        legOdd.append(float(leginfo[0]))
+        legFuel.append(float(leginfo[1])) 
+        line = infile.readline()
+        line = line.strip()
+    infile.close()
+
+    for i in range(len(legOdd)):
+        legDistance.append(legOdd[i] - currentOdometer)
+        totalMileage = totalMileage + legDistance[i]
+        totalFuel = totalFuel + legFuel[i]
+        currentOdometer = legOdd[i]
+        print(f"Leg {i+1} was {legDistance[i]} miles. {legFuel[i]} gallons was used. The MPG was {round((legDistance[i] / legFuel[i]),2)} miles per gallon. ") 
+    
+    print(f"The total distance was {round(totalMileage,2)} miles. The total MPG was {round((totalMileage / totalFuel),2)}.")
 
 
 
 ##################################################################################################################
+
 
 def main():
-    print()
+    startOdometerReading = obtainStartingOdometer()
+    calculateTripMileage(startOdometerReading)
+
+
 
 main()
--- CS120/Assignments/Assignment #11/Thomas_Crow_Assignment11-5.py
+++ CS120/Assignments/Assignment #11/Thomas_Crow_Assignment11-5.py
@@ -16,16 +16,62 @@
 #
 
 ##################################################################################################################
-#  Description: 
-#  Input: 
-#  Output:
+#  Description:  Prints truth table based on user inputed boolean expression 
+#  Input: Expression from user as string list
+#  Output: None
 
+def printTruthTable(boolExpression):
 
+    variables = []
+    duplicate = False
 
+    boolExpression = boolExpression.strip()
+    expression = boolExpression.split(' ')
+    for i in range(len(expression)):
+        if (expression[i] != "and") and (expression[i] != "or") and (expression[i] != "not"):
+            for j in range(len(variables)):
+                if expression[i] == variables[j]:
+                    duplicate = True
+            if duplicate == False:
+                variables.append(expression[i])
+            duplicate = False
+    
+    if len(variables) > 2:
+        print("Please only enter up to two variables.")
+        return
+    
+    if len(variables) == 0:
+        print("Please at least one variable.")
+        return
+    
+ 
+    if len(variables) == 2:
+        locals()[variables[0]] = True
+        locals()[variables[1]] = True
+        print(f"|   {variables[0]} \t\t\t|   {variables[1]} \t\t\t|   {boolExpression} \t\t\t|")
+        print(f"|   {eval(variables[0])} \t\t|   {eval(variables[1])} \t\t|   {eval(boolExpression)} \t\t\t|")
+        locals()[variables[0]] = True
+        locals()[variables[1]] = False
+        print(f"|   {eval(variables[0])} \t\t|   {eval(variables[1])} \t\t|   {eval(boolExpression)} \t\t\t|")
+        locals()[variables[0]] = False
+        locals()[variables[1]] = True
+        print(f"|   {eval(variables[0])} \t\t|   {eval(variables[1])} \t\t|   {eval(boolExpression)} \t\t\t|")
+        locals()[variables[0]] = False
+        locals()[variables[1]] = False
+        print(f"|   {eval(variables[0])} \t\t|   {eval(variables[1])} \t\t|   {eval(boolExpression)} \t\t\t|")
+
+    if len(variables) == 1:
+        locals()[variables[0]] = True
+        print(f"|   {variables[0]} \t\t\t|   {boolExpression} \t\t\t|")
+        print(f"|   {eval(variables[0])} \t\t|  {eval(boolExpression)} \t\t\t|")
+        locals()[variables[0]] = False
+        print(f"|   {eval(variables[0])} \t\t|  {eval(boolExpression)} \t\t\t|")
+        
 
 ##################################################################################################################
 
 def main():
-    print()
+    booleanExpression = input("Please enter the boolean expression: ")
+    printTruthTable(booleanExpression)
 
 main()
