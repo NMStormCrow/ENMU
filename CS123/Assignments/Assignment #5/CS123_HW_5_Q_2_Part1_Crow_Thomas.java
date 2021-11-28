538bcf81ce7f620a9afa38bf28a2ac4966f05c19
Thomas Crow <paladin677@gmail.com>
2021-11-28 14:02:51 -0700

    Created hw files

---
+++ CS123/Assignments/Assignment #5/CS123_HW_5_Q_2_Part1_Crow_Thomas.java
@@ -0,0 +1,17 @@
+// Assignment: Homework #5 Question #1 
+// Creator: Thomas Crow
+// Class:  CS123
+// Date: 11/28/2021
+// Requirements: Write a Java method “printNTimes” to print a given string for a given times. 
+//               Your method should accept two inputs, one is String s and another is an integer n. 
+//               The input string s will be printed n times. What is the return type? 
+
+
+
+import java.util.Scanner;
+
+class PrintWorldViaForLoop {
+    public static void main(String args[]) {
+        
+
+}
