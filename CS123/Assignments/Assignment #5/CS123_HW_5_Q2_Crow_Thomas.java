1423b0e747063b6f9c7d8f9e70d92839feefcdc4
Thomas Crow <paladin677@gmail.com>
2021-11-28 22:44:09 -0700

    Completed the second problem

--- CS123/Assignments/Assignment #5/CS123_HW_5_Q2_Crow_Thomas.java
+++ CS123/Assignments/Assignment #5/CS123_HW_5_Q2_Crow_Thomas.java
@@ -1,4 +1,4 @@
-// Assignment: Homework #5 Question #1 
+// Assignment: Homework #5 Question #2 
 // Creator: Thomas Crow
 // Class:  CS123
 // Date: 11/28/2021
@@ -9,8 +9,34 @@
 
 import java.util.Scanner;
 
-class PrintWorldViaForLoop {
+class LargestOfFour {
+
+    static int getLargestNumber(int num1, int num2, int num3, int num4) {
+        int largestNum = num1;
+        if (num2 > largestNum) {
+            largestNum = num2;
+        }
+        if (num3 > largestNum) {
+            largestNum = num3;
+        }
+        if (num4 > largestNum) {
+            largestNum = num4;
+        }
+        return largestNum;
+    }
     public static void main(String args[]) {
-        
-       
+        System.out.println("This program determines the largest of four numbers entered.");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter the first number: ");
+        int userNum1 = scan.nextInt();
+        System.out.print("Please enter the second number: ");
+        int userNum2 = scan.nextInt();
+        System.out.print("Please enter the third number: ");
+        int userNum3 = scan.nextInt();
+        System.out.print("Please enter the fourth number: ");
+        int userNum4 = scan.nextInt();
+        int largestNumber = getLargestNumber(userNum1, userNum2, userNum3, userNum4);
+        System.out.println("The largest number is " + largestNumber);
+        scan.close();
+    }   
 }
