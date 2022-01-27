4370fa5beb6db8871ec75a5de18831236561e174
Thomas Crow <paladin677@gmail.com>
2022-01-26 23:20:27 -0700

    Updated Bank Account exercise

--- CS357/Exercises/BankAccount.java
+++ CS357/Exercises/BankAccount.java
@@ -1,8 +1,17 @@
 import java.util.Scanner;
 
-public class BankAccount {
+class BankAccount {
+
+    private double balance;  //account balance
+
+    public BankAccount(double openingBalance)  { //constructor
+        balance = openingBalance;
+    }
     
-    
+    public void deposit(double amount) { //makes depsoit
+        balance = balance + amount;
+    }
+
     
     public static void main(String args[]) {
         
