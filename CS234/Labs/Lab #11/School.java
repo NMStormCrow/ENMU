8b24b5945985c1d7f41736e4fce49744da82bdb7
Thomas Crow <paladin677@gmail.com>
2022-04-09 16:29:11 -0600

    Working on Lab #11 for CS234

--- CS234/Labs/Lab #11/School.java
+++ CS234/Labs/Lab #11/School.java
@@ -4,7 +4,33 @@
 * CS234
 */
 
+import java.util.HashMap;
 
 public class School {
-    
+
+
+//  Declare instance variables
+    HashMap<String, Student> students = new HashMap<>();
+
+//  Default constructor
+    public School() {
+
+    }
+
+//  Method: addStudent()
+//  Purpose: 
+//  Arguments: 
+//  Returns: 
+
+//  Method:    generateGrades()
+//  Purpose:   Generates five random grades for a given student
+//  Arguments: (The student as a class instance of student,
+//             the minimum and maxium value of the random grade values as doubles)
+//  Returns:   None
+
+//  Method: getStudentInfo
+//  Purpose: 
+//  Arguments: 
+//  Returns: None
+
 }
